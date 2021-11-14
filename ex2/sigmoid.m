16cbfd6d861eebd21d890ae86bf9669bb964be0d
GadagiAmogh <amoghgadagi@gmail.com>
2021-11-13 17:13:27 -0700

    Adding code for 2nd exercise

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -8,7 +8,8 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
+e = exp(-z);
+g = 1./(1+e);
 
 
 
